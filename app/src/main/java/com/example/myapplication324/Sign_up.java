52dff5a78c154f2aa023cd758e468ad1610b48eb
Sarahkhj <xladysarah@gmail.com>
2023-09-27 22:51:34 +0300

    hi

--- app/src/main/java/com/example/myapplication324/Sign_up.java
+++ app/src/main/java/com/example/myapplication324/Sign_up.java
@@ -5,38 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button b1;
+public class Sign_up extends AppCompatActivity {
     private TextView t1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        b1 = (Button) findViewById(R.id.join_us);
+        setContentView(R.layout.activity_sign_up);
         t1 = (TextView) findViewById(R.id.login);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                opensignup();
-            }
-        });
-
         t1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openlogin();
             }
         });
-    }
 
-    public void opensignup(){
-        Intent intent = new Intent(this, Sign_up.class);
-        startActivity(intent);
+
     }
 
     public void openlogin(){
