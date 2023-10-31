a1238908f1d72fdbe497dbdcb346d06b507b2bd0
Sarahkhj <xladysarah@gmail.com>
2023-10-31 21:24:40 +0300

    Nav

--- app/src/main/java/com/example/myapplication324/Home.java
+++ app/src/main/java/com/example/myapplication324/Home.java
@@ -1,14 +1,23 @@
 package com.example.myapplication324;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,6 +29,15 @@ public class Home extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseDatabase rootNode;
     private String rtvFullName;
+    protected final int home = 1;
+    protected final int favo = 2;
+    protected final int shared = 3;
+    protected final int search = 4;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +47,7 @@ public class Home extends AppCompatActivity {
         b1 = findViewById(R.id.logout);
         auth = FirebaseAuth.getInstance();
         rootNode =FirebaseDatabase.getInstance();
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,6 +67,49 @@ public class Home extends AppCompatActivity {
         } else {
             StyleableToast.makeText(Home.this, "Error = No users Found !", Toast.LENGTH_SHORT,R.style.mytoast).show();
         }
+//Bottom nav
+        MeowBottomNavigation bottomNavigation = findViewById(R.id.meow);
+        bottomNavigation.add(new MeowBottomNavigation.Model(home,R.drawable.baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(search,R.drawable.baseline_search_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(favo,R.drawable.baseline_favorite_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(shared,R.drawable.baseline_group_24));
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                StyleableToast.makeText(Home.this, "item"+item.getId(), Toast.LENGTH_SHORT,R.style.mytoast).show();
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                String name;
+                switch(item.getId()){
+                    case home:name="home";
+                    break;
+                    case favo:name="favo";
+                        break;
+                    case shared:name="shared";
+                        break;
+                    case search:name="search";
+                        Intent intent = new Intent(Home.this, search.class);
+                        startActivity(intent);
+                        break;                }
+            }
+
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+                // your codes
+            }
+        });
+
+
+
 
     }
+
+
 }
