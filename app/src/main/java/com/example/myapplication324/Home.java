63e6d5cb6ab39249bcdc29dc19f66dc884842614
noufd22 <noufdnouf22@gmail.com>
2023-11-03 19:39:14 +0300

    upload files

--- app/src/main/java/com/example/myapplication324/Home.java
+++ app/src/main/java/com/example/myapplication324/Home.java
@@ -1,30 +1,27 @@
 package com.example.myapplication324;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.nfc.Tag;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
-import com.example.myapplication324.databinding.ActivityDashboardBinding;
 import com.example.myapplication324.databinding.ActivityHomeBinding;
-import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.github.muddz.styleabletoast.StyleableToast;
 
@@ -39,8 +36,8 @@ public class Home extends DrawerBaseActivity { //i changed the extends class
     protected final int shared = 3;
     protected final int search = 4;
 
-     private Button chooseFile_btn;
-     private TextView filePath;
+    private Button chooseFile_btn;
+    private TextView filePath;
 
     private final int CHOSE_PDF_FROM_DEVICE=1001;
     private final int PICK_WORD_FILE=1002;
@@ -48,8 +45,13 @@ public class Home extends DrawerBaseActivity { //i changed the extends class
     private static final String TAG ="Home";
     ActivityHomeBinding activityHomeBinding;
 
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
+    private String currentUserId; // This should be unique for each user
 
 
+    private List<FileMetadata> fileMetadataList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +68,12 @@ public class Home extends DrawerBaseActivity { //i changed the extends class
 
         chooseFile_btn= findViewById(R.id.choose_file_btn);
         filePath = findViewById(R.id.file_path);
+
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        currentUserId = auth.getCurrentUser().getUid(); // You should have a unique identifier for each user.
 
         chooseFile_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -139,55 +147,102 @@ public class Home extends DrawerBaseActivity { //i changed the extends class
             }
         });
 
-
-
-
     }
-   private void callChoosePdfFile(){
+
+    ////Uploading files////
+    private void callChoosePdfFile(){
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("application/pdf");
-        startActivityForResult(intent,CHOSE_PDF_FROM_DEVICE);
-   }
-
-   public void onActivityResult(int requestCode, int resultCode, Intent resultDate) {
-       super.onActivityResult(requestCode, resultCode, resultDate);
-       if (requestCode ==CHOSE_PDF_FROM_DEVICE && resultCode == Activity.RESULT_OK){
-
-           if(resultDate != null){
-               Log.d(TAG,"onActivityResult: "+resultDate.getData());
-
-               filePath.setText("File Path: "+resultDate.getData());
-
-           }
-       }
-
-       if (requestCode ==PICK_WORD_FILE && resultCode == Activity.RESULT_OK){
-
-           if(resultDate != null){
-               Log.d(TAG,"onActivityResult: "+resultDate.getData());
-
-               filePath.setText("File Path: "+resultDate.getData());
-
-           }
-       }
-
-
-   }
-   private void callChooseWordFile(){
-
-       Intent intent1 = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-       intent1.addCategory(Intent.CATEGORY_OPENABLE);
-       intent1.setType("*/*");
-       String[] mimetype = {"application/vnd.openxmlformats-officedocument.wordprocessingml.document",""};
-       intent1.putExtra(Intent.EXTRA_MIME_TYPES,mimetype);
-       startActivityForResult(intent1,PICK_WORD_FILE);
-
-   }
-    private void callChooseTXTfFile(){
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("text/plain");
-        startActivityForResult(intent,CHOSE_PDF_FROM_DEVICE);
+        startActivityForResult(intent, CHOSE_PDF_FROM_DEVICE);
     }
+    private void callChooseWordFile(){
+
+        Intent intent1 = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent1.addCategory(Intent.CATEGORY_OPENABLE);
+        intent1.setType("*/*");
+        String[] mimetype = {"application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"};
+        intent1.putExtra(Intent.EXTRA_MIME_TYPES,mimetype);
+        startActivityForResult(intent1,PICK_WORD_FILE);
+
+    }
+    //    private void callChooseTXTfFile(){
+//        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+//        intent.addCategory(Intent.CATEGORY_OPENABLE);
+//        intent.setType("text/plain");
+//        startActivityForResult(intent,CHOSE_PDF_FROM_DEVICE);
+//    }
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (requestCode ==CHOSE_PDF_FROM_DEVICE && resultCode == Activity.RESULT_OK){
+
+            if (resultData != null) {
+                Uri fileUri = resultData.getData();
+                if (fileUri != null) {
+                    String fileName =  getFileNameFromUri(fileUri); // Get the original file name
+                    StorageReference fileReference = storageReference.child(currentUserId + "/" + fileName);
+
+                    UploadTask uploadTask = fileReference.putFile(fileUri);
+                    uploadTask.addOnSuccessListener(taskSnapshot -> {
+                        StyleableToast.makeText(Home.this, "PDF uploaded successfully!", Toast.LENGTH_SHORT, R.style.mytoast).show();
+
+                        // Get the download URL of the uploaded file
+                        fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
+                            String fileDownloadUrl = uri.toString();
+
+                            // Store file metadata in the Realtime Database
+                            FileMetadata fileMetadata = new FileMetadata(fileName, fileDownloadUrl);
+                            databaseReference.child("files").child(currentUserId).push().setValue(fileMetadata);
+                        });
+                    }).addOnFailureListener(e -> {
+                        StyleableToast.makeText(Home.this, "Failed to upload PDF: " + e.getMessage(), Toast.LENGTH_SHORT, R.style.mytoast).show();
+                    });
+
+                }
+            }}else if (requestCode == PICK_WORD_FILE && resultCode == Activity.RESULT_OK) {
+            if (resultData != null) {
+                Uri fileUri = resultData.getData();
+                if (fileUri != null) {
+                    String fileName =  getFileNameFromUri(fileUri); // Get the original file name
+                    StorageReference fileReference = storageReference.child(currentUserId + "/" + fileName);
+
+                    UploadTask uploadTask = fileReference.putFile(fileUri);
+                    uploadTask.addOnSuccessListener(taskSnapshot -> {
+                        StyleableToast.makeText(Home.this, "Word file uploaded successfully!", Toast.LENGTH_SHORT, R.style.mytoast).show();
+
+                        // Get the download URL of the uploaded file
+                        fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
+                            String fileDownloadUrl = uri.toString();
+
+                            // Store file metadata in the Realtime Database
+                            FileMetadata fileMetadata = new FileMetadata(fileName, fileDownloadUrl);
+                            databaseReference.child("files").child(currentUserId).push().setValue(fileMetadata);
+                        });
+                    }).addOnFailureListener(e -> {
+                        StyleableToast.makeText(Home.this, "Failed to upload Word file: " + e.getMessage(), Toast.LENGTH_SHORT, R.style.mytoast).show();
+                    });
+                }
+            }
+        }
+    }
+
+    private String getFileNameFromUri(Uri uri) {
+        String fileName = "unknown";
+        Cursor cursor = null;
+        try {
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int displayNameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                if (displayNameIndex != -1) {
+                    fileName = cursor.getString(displayNameIndex);
+                }
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return fileName;
+    }
+
 }
